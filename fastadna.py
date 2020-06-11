c4fba43414b91160f46ef6b0f45a62bb6909dcad
Robert Ciemny <emeryt360@gmail.com>
2020-06-12 00:48:23 +0200

    Add view option

--- fastadna.py
+++ fastadna.py
@@ -1,4 +1,5 @@
 import argparse
+import re
 
 def print_lengths(file_name, extra = False):
   f = open(file_name, "r")
@@ -20,12 +21,54 @@ def print_lengths(file_name, extra = False):
       
   print(line_len)
 
+def view(file_name, arg):
+  f = open(file_name, "r")
+  data = ''
+
+  if(len(arg) > 1):
+    coords = list(map(int,arg[1].split(':')))
+    if(coords[1] <= coords[0]):
+      print("Error: End <= Start!")
+    else:
+      found = False
+      for line in f:
+        if(found):
+          
+          if(line[0] == ">"):
+            break
+          elif(line[0] == '\n'):
+            continue
+          else:
+            data+=line[:-1]
+            if (len(data) >= coords[1]):
+              print(data[coords[0]:coords[1]])
+              break
+
+        if(line[0] == ">"):
+          if(arg[0] == line.split()[0][1:]):
+            found = True
+  else:
+    found = False
+    for line in f:
+      if(found):
+        if(line[0] == ">"):
+          break
+        else:
+          print(line, end = '')
+      if(line[0] == ">"):
+        if(arg[0] == line.split()[0][1:]):
+          found = True
+
 def run():
   parser = argparse.ArgumentParser()
-  parser.add_argument("-f","--file", help="name of the file with extension in program catalog or path to file. ex: fastadna.py --file test.fa.")
-  parser.add_argument("-l","--length", help="shows length of all sequences. Use ONLY with --file [-f].", action="store_true")
+  group = parser.add_mutually_exclusive_group()
+  parser.add_argument("file", help="name of the file with extension in program catalog or path to file.")
+
+  group.add_argument("-l","--length", help="shows length of all sequences.", action="store_true")
+  group.add_argument("-v", "--view", nargs='+', help="shows sequence.")
+
   parser.add_argument("-enl", "--extranl", help="prints extra new lines between results of --length [-l].", action="store_true")
-  parser.add_argument("-v", "--version", action="version", version="%(prog)s 0.1")
+  parser.add_argument("-ver", "--version", action="version", version="%(prog)s 0.1")
 
   args = parser.parse_args()
 
@@ -35,6 +78,8 @@ def run():
         print_lengths(args.file, True)
       else:
         print_lengths(args.file)
+    if args.view:
+      view(args.file, args.view)
   if args.length and not args.file:
     parser.error("File should be provided first, use --help [-h] for more information.")
   if args.extranl and not args.length:
