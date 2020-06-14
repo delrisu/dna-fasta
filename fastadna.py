b38d433e0a02370abe14c8650eedb4aa6599fa35
Robert Ciemny <emeryt360@gmail.com>
2020-06-14 18:55:43 +0200

    Add insert & insert_random

--- fastadna.py
+++ fastadna.py
@@ -1,16 +1,20 @@
 import argparse
-import re
 import textwrap
+import random
 
 def print_long(table):
   for i in range(0, len(table)):
-    if(i > 0 and i%70 == 0):
+    if((i+1)%70 == 0):
       print(table[i])
     else:
       print(table[i], end='')
 
 def slice_middle(table, arg):
   table = table[:arg[0]] + table[arg[1]:]
+  print_long(table)
+
+def insert_fragment(table, start, fragment):
+  table = table[:start] + fragment + table[start:]
   print_long(table)
 
 def print_lengths(file_name, extra = False):
@@ -71,6 +75,8 @@ def view(file_name, arg):
         if(found):
           if(line[0] == ">"):
             break
+          elif(line[0] == "\n"):
+            continue
           else:
             print(line, end = '')
         if(line[0] == ">"):
@@ -109,6 +115,27 @@ def delete(file_name, arg):
       if(found and not outed):
         slice_middle(data,coords)
 
+def insert(file_name, arg):
+  with open(file_name, "r") as f:
+    data = ''
+    found = False
+    for line in f:
+      if(found):
+        if(line[0] == ">"):
+          break
+        elif(line[0] == "\n"):
+          continue
+        else:
+          data+=line[:-1]
+      if(line[0] == ">"):
+        if(arg[0] == line.split()[0][1:]):
+          found = True
+    insert_fragment(data,int(arg[1]),arg[2])
+
+def insert_random(file_name, arg):
+  pass_arg = [arg[0], arg[1], ''.join(random.choices("ACGT", k=int(arg[2])))]
+  insert(file_name, pass_arg)
+
 def run():
   parser = argparse.ArgumentParser()
   group = parser.add_mutually_exclusive_group()
@@ -117,6 +144,8 @@ def run():
   group.add_argument("-l","--length", help="shows length of all sequences.", action="store_true")
   group.add_argument("-v", "--view", nargs='+', help="Shows sequence. Slice with 'name start:end'")
   group.add_argument("-d", "--delete", nargs=2, help="Shows sequence with part deleted. 'name start:end'")
+  group.add_argument("-i", "--insert", nargs=3, help="Shows sequence with added part. 'name start additional_sequence'")
+  group.add_argument("-ir", "--insert_random", nargs=3, help="Shows sequence with added random part. 'name start length'")
 
   parser.add_argument("-enl", "--extranl", help="prints extra new lines between results of --length [-l].", action="store_true")
   parser.add_argument("-ver", "--version", action="version", version="%(prog)s 0.1")
@@ -133,6 +162,10 @@ def run():
       view(args.file, args.view)
     elif args.delete:
       delete(args.file, args.delete)
+    elif args.insert:
+      insert(args.file, args.insert)
+    elif args.insert_random:
+      insert_random(args.file, args.insert_random)
   if args.length and not args.file:
     parser.error("File should be provided first, use --help [-h] for more information.")
   if args.extranl and not args.length:
